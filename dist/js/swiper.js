1db73aa5878444d8f06d4894d10512051dff96e8
MoisesDB <75042157+MoisesDB@users.noreply.github.com>
2021-06-14 18:45:09 -0300

    dist commit

---
+++ dist/js/swiper.js
@@ -0,0 +1,1 @@
+const swiper=new Swiper(".swiper-container",{direction:"horizontal",loop:true,pagination:{el:".swiper-pagination",},autoplay:{delay:3000,},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev",},});
